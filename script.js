dff6dded15c2fb00f32fb629b338bba3fcb218be
IronmanRakesh <rakeshlanderi.199726@gmail.com>
2024-10-24 14:41:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function openPopup(popupId) {
-        document.getElementById(popupId).style.display = "block";
+function openPopup() {
+        document.getElementById("project1-popup").style.display = "block";
     }
     
-    function closePopup(popupId) {
-        document.getElementById(popupId).style.display = "none";
+    function closePopup() {
+        document.getElementById("project1-popup").style.display = "none";
     }
+//Close modal if clicked outside of the modal-content
+window.onclick = function(event){
+    var modal = document.getElementById("project1-popup");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function openPopup2() {
+    document.getElementById("project2-popup").style.display = "block";
+}
+
+function closePopup2() {
+    document.getElementById("project2-popup").style.display = "none";
+}
+//Close modal if clicked outside of the modal-content
+window.onclick = function(event){
+var modal = document.getElementById("project2-popup");
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
